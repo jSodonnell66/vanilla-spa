af9146f6e7b1d157dfcd3ddcaf4924b16fb7b080
Jack O'Donnell <jack.odonnell@developersinstitute.co.nz>
2023-09-29 11:08:25 +1300

    correction to server file endsWith .js to resolve browser console error

--- frontend/static/js/views/Dashboard.js
+++ frontend/static/js/views/Dashboard.js
@@ -2,6 +2,7 @@ import AbstractView from "./AbstractView";
 
 export default class extends AbstractView {
   constructor() {
+    super();
     this.setTitle("Dashboard");
   }
 
--- server.js
+++ server.js
@@ -3,9 +3,16 @@ const path = require("path");
 
 const app = express();
 
+// Serve your static assets (including JavaScript files) with the correct MIME type
 app.use(
   "/static",
-  express.static(path.resolve(__dirname, "frontend", "static"))
+  express.static(path.resolve(__dirname, "frontend", "static"), {
+    setHeaders: (res, filePath) => {
+      if (filePath.endsWith(".js")) {
+        res.setHeader("Content-Type", "text/javascript");
+      }
+    },
+  })
 );
 
 app.get("/*", (req, res) => {
